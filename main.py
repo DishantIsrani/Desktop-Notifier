6d8e5f7e6183ec286dc29acc17c5022af9f04338
Dishant Israni <isranidishant14@gmail.com>
2021-11-16 00:33:51 +0530

    Adding background and minor UI changes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from tkinter import *
 from plyer import notification
 from tkinter import messagebox
+from PIL import Image, ImageTk
 import time
 
 
 t = Tk()
 t.title('Dishant-Notifier-app')
-t.geometry('500x300')
+t.geometry('500x400')
+img = Image.open("notify.png")
+tkimage = ImageTk.PhotoImage(img)
 
 def get_details():
     get_title = title.get()
@@ -29,29 +32,31 @@ def get_details():
             timeout = 10
         )
 
+img_label = Label(t, image=tkimage).grid()
+
 t_label = Label(t, text="Title to Notify",font=("sans-serif", 10))
 t_label.place(x=12, y=70)
 
-title = Entry(t, width="25",font=("sans-serif", 13))
+title = Entry(t, width="25",font=("sans-serif", 13), bg='grey')
 title.place(x=123, y=70)
 
 
 m_label = Label(t, text="Display Message", font=("sans-serif", 10))
 m_label.place(x=12, y=120)
 
-msg = Entry(t, width="40", font=("sans-serif", 13))
+msg = Entry(t, width="25", font=("sans-serif", 13), bg='grey')
 msg.place(x=123,height=30, y=120)
 
 
 time_label = Label(t, text="Set Time", font=("sans-serif", 10))
 time_label.place(x=12, y=175)
 
-time1 = Entry(t, width="5", font=("sans-serif", 13))
+time1 = Entry(t, width="25", font=("sans-serif", 13), bg='grey')
 time1.place(x=123, y=175)
 
 
-time_min_label = Label(t, text="min", font=("sans-serif", 10))
-time_min_label.place(x=175, y=180)
+time_min_label = Label(t, text="min", font=("sans-serif", 10), bg='grey')
+time_min_label.place(x=355, y=175)
 
 
 but = Button(t, text="SET NOTIFICATION", font=("sans-serif", 10, "bold"), fg="#ffffff", bg="#528DFF", width=20,
